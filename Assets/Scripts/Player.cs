e8f5815df5e7f6fcf190ae70e7b0b346336ce8aa
Ihor Zhurakovskyi <ihor.zhurakovskyi@ab-games.com>
2022-04-24 01:26:59 +0300

    In progress
    Add campfire, update terrain, add new materials

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,11 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using DG.Tweening;
+using Unity.Mathematics;
 using UnityEngine;
 
 public class Player : MonoBehaviour
 {
     [SerializeField] private Animator _playerAnim;
+    [SerializeField] private GameObject _hitParticle;
+    [SerializeField] private Transform _hitAnchor;
+
     private bool _isBlocked = false;
 
     public bool IsBlocked => _isBlocked;
@@ -13,10 +17,16 @@ public class Player : MonoBehaviour
     public void GetHit()
     {
         _playerAnim.SetTrigger("GetHit");
+        CreateHitParticle();
         _isBlocked = true;
         DOVirtual.DelayedCall(0.5f, () =>
         {
             _isBlocked = false;
         });
+    }
+
+    private void CreateHitParticle()
+    {
+        Instantiate(_hitParticle, _hitAnchor.position, quaternion.identity,_hitAnchor);
     }
 }
